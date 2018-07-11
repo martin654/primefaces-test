4c8da3c2e303ae5052f83b497f70eb29dccbfe88
martin654 <svihlik.m@gmail.com>
2018-07-11 10:40:49 +0200

    Datatable select all doesnt't update the table

--- src/main/java/org/primefaces/test/TestViewCdi.java
+++ src/main/java/org/primefaces/test/TestViewCdi.java
@@ -1,5 +1,8 @@
 package org.primefaces.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
@@ -7,19 +10,59 @@ import javax.inject.Named;
 @Named
 @RequestScoped
 public class TestViewCdi {
-    
-    private String testString;
-    
-    @PostConstruct  
-    public void init() {
-        testString = "Welcome to PrimeFaces (from CDI!) !!!";
-    }
 
-    public String getTestString() {
-        return testString;
-    }
+  private String testString;
 
-    public void setTestString(String testString) {
-        this.testString = testString;
-    }    
+  private List<String> testList = new ArrayList<>();
+
+  private List<String> testListSelected = new ArrayList<>();
+
+  @PostConstruct
+  public void init() {
+    testString = "Welcome to PrimeFaces (from CDI!) !!!";
+
+    for (int i = 0; i < 50; i++) {
+      getTestList().add("Foooo " + i);
+    }
+  }
+
+  public void fooAction() {
+    System.out.println("FOOO");
+  }
+
+  public String getTestString() {
+    return testString;
+  }
+
+  public void setTestString(String testString) {
+    this.testString = testString;
+  }
+
+  /**
+   * @return the testListSelected
+   */
+  public List<String> getTestListSelected() {
+    return testListSelected;
+  }
+
+  /**
+   * @param testListSelected the testListSelected to set
+   */
+  public void setTestListSelected(List<String> testListSelected) {
+    this.testListSelected = testListSelected;
+  }
+
+  /**
+   * @return the testList
+   */
+  public List<String> getTestList() {
+    return testList;
+  }
+
+  /**
+   * @param testList the testList to set
+   */
+  public void setTestList(List<String> testList) {
+    this.testList = testList;
+  }
 }
